9fe34c5e8717ef22663e6e58cc387d46b8d95d88
MyPc <MyPc@172.16.108.179>
2016-06-20 00:56:14 +0500

    A

---
+++ src/myfinalprojectanu/calculation.java
@@ -0,0 +1,13 @@
+package myfinalprojectanu;
+
+public class calculation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+submittion add=new submittion();
+int a=5;
+int b=5;
+add.sub(a, b);
+	}
+
+}
---
+++ src/myfinalprojectanu/submittion.java
@@ -0,0 +1,9 @@
+package myfinalprojectanu;
+
+public class submittion {
+int sub(int a,int b){
+	int sum=0;
+	sum=a+b;
+	return sum;
+}
+}
