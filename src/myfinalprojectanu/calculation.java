ae49dbb742da0a665b6963dc5f47e13304034784
MyPc <MyPc@172.16.108.179>
2016-06-20 00:59:23 +0500

    B

--- src/myfinalprojectanu/calculation.java
+++ src/myfinalprojectanu/calculation.java
@@ -5,9 +5,11 @@ public class calculation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 submittion add=new submittion();
+subtract s=new subtract();
 int a=5;
 int b=5;
 add.sub(a, b);
+s.subt(a, b);
 	}
 
 }
